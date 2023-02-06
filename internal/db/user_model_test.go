6a2f057f9aa57faba74973cdbbcd352c8bee1b42
anax <anax@oikos>
2023-02-06 11:08:56 -0500

    update user model tests

--- internal/db/user_model_test.go
+++ internal/db/user_model_test.go
@@ -11,8 +11,8 @@ import (
 func TestCreateUser(t *testing.T) {
 	db, err := NewDatabase("")
 	require.NoError(t, err)
-	var username = "test"
-	var password = "test"
+	var username = "test1"
+	var password = "test1"
 
 	usr, err := db.CreateUser(context.Background(), username, password)
 	require.NoError(t, err)
@@ -23,34 +23,42 @@ func TestGetUserByID(t *testing.T) {
 	db, err := NewDatabase("")
 	require.NoError(t, err)
 
-	usr, err := db.GetUserByID(context.Background(), "1")
+	usr, err := db.GetUserByID(context.Background(), "2")
 	require.NoError(t, err)
 	require.NotEmpty(t, usr)
-	require.Equal(t, usr.ID, "1")
+	require.Equal(t, usr.ID, "2")
 }
 
 func TestGetUserByName(t *testing.T) {
 	db, err := NewDatabase("")
 	require.NoError(t, err)
 
-	usr, err := db.GetUserByName(context.Background(), "test")
+	usr, err := db.GetUserByName(context.Background(), "test1")
 	require.NoError(t, err)
 	require.NotEmpty(t, usr)
-	require.Equal(t, usr.UserName, "test")
+	require.Equal(t, usr.UserName, "test1")
 }
 
 func TestUpdateUserPassword(t *testing.T) {
 	db, err := NewDatabase("")
 	require.NoError(t, err)
 
-	passwordChangeErr := db.UpdateUserPassword(context.Background(), "test", "test")
+	passwordChangeErr := db.UpdateUserPassword(context.Background(), "test1", "password")
 	require.NoError(t, passwordChangeErr)
+}
+
+func TestLogin(t *testing.T) {
+	db, err := NewDatabase("")
+	require.NoError(t, err)
+
+	_, err = db.Login(context.Background(), "test1", "test1")
+	require.NoError(t, err)
 }
 
 func TestDeleteUser(t *testing.T) {
 	db, err := NewDatabase("")
 	require.NoError(t, err)
 
-	deleteUserErr := db.DeleteUser(context.Background(), "test")
+	deleteUserErr := db.DeleteUser(context.Background(), "test1")
 	require.NoError(t, deleteUserErr)
 }
